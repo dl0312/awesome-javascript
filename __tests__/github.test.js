41631a59099aecc657efc79bfd9f987b3d42622a
GeonLee <leegun2003@gmail.com>
2019-06-01 22:18:56 +0900

    chore: update engines in package.json

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -5,7 +5,6 @@ describe("Integration with GitHub API", () => {
   let github;
 
   beforeAll(() => {
-    console.log(process.env.ACCESS_TOKEN);
     github = new GitHub({
       accessToken: process.env.ACCESS_TOKEN,
       baseURL: "https://api.github.com"
