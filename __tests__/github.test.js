47722edf011f43f3ca2de14ba16e6024120c83c9
GeonLee <leegun2003@gmail.com>
2019-06-01 22:17:17 +0900

    fix: add .env config

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -1,9 +1,11 @@
 const GitHub = require("../lib/github");
+require("dotenv").config();
 
 describe("Integration with GitHub API", () => {
   let github;
 
   beforeAll(() => {
+    console.log(process.env.ACCESS_TOKEN);
     github = new GitHub({
       accessToken: process.env.ACCESS_TOKEN,
       baseURL: "https://api.github.com"
